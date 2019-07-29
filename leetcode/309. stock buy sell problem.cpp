581580ec89325cbbf7c99e11558893a9dc8f61d2
Mona-su <35690236+Mona-su@users.noreply.github.com>
2019-07-29 23:40:12 +0800

    Create 309. stock buy sell problem.cpp

---
+++ leetcode/309. stock buy sell problem.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int length = prices.size();
+        if (length > 1){
+            // max profit on day i, 0 means the day before the first given day
+            // for state: 0 means not holding stock, 1 means holding stock, 2 not holding stock for more than one day
+            int sol[length][3];
+            
+            // initialization
+            sol[0][0] = 0;
+            sol[0][1] = -prices[0];
+            sol[0][2] = 0;
+            
+            for (int i = 1; i < length; i++){
+                sol[i][0] = max(sol[i-1][0], sol[i-1][1] + prices[i]);
+                sol[i][1] = max(sol[i-1][1], sol[i-1][2] - prices[i]);
+                sol[i][2] = sol[i-1][0];
+            }
+            return max(sol[length-1][0], sol[length-1][2]);
+        }
+        return 0;
+    }
+};
